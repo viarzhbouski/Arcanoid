19bf654fcf62de58947989b868dd3339b4a1eb6a
viarzhbouski <lordsith1996@gmail.com>
2022-03-18 15:57:38 +0300

    +

--- Assets/Scripts/MonoModels/BlockMono.cs
+++ Assets/Scripts/MonoModels/BlockMono.cs
@@ -1,6 +1,9 @@
-﻿using Common.Enums;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Common.Enums;
 using Scripts.ScriptableObjects;
 using UnityEngine;
+using DG.Tweening;
 
 namespace MonoModels
 {
@@ -8,6 +11,9 @@ namespace MonoModels
     {
         [SerializeField]
         private SpriteRenderer spriteRenderer;
+        
+        [SerializeField]
+        private List<Sprite> damageSprites;
         
         private Block _block;
 
@@ -17,15 +23,56 @@ namespace MonoModels
 
         public bool CanDestroy =>  _block.BlockType != BlockTypes.Undestroyable && _block.HealthPoints <= 0;
 
+        private int _damageForChangeSprite;
+        private int _damageSum;
+        private Queue<Sprite> _spriteQueue;
+        
+
         public void SetBlockConfig(Block block)
         {
             _block = block;
             spriteRenderer.color = block.Color;
+
+            if (_block.HealthPoints > damageSprites.Count)
+            {
+                _damageForChangeSprite = _block.HealthPoints / damageSprites.Count;
+            }
+            else
+            {
+                _damageForChangeSprite = 1;
+            }
+            
+            
+            _spriteQueue = new Queue<Sprite>();
+            _damageSum = 0;
+            
+            foreach (var damageSprite in damageSprites)
+            {
+                _spriteQueue.Enqueue(damageSprite);
+            }
         }
 
         public void Damage()
         {
             _block.HealthPoints--;
+            _damageSum++;
+            
+            PlayDamageAnim();
+            ChangeSprite();
         }
+
+        private void PlayDamageAnim()
+        {
+            transform.DOShakePosition(0.05f, 0.5f).SetEase(Ease.OutBounce);
+        }
+
+        private void ChangeSprite()
+        {
+            if (_block.BlockType != BlockTypes.Undestroyable &&_damageSum % _damageForChangeSprite == 0 && _spriteQueue.Any())
+            {
+                spriteRenderer.sprite = _spriteQueue.Dequeue();
+            }
+        }
+        
     }
 }
